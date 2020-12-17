deb95010f3663863dd03246c42f39e74e757b40f
Kim Seongchan <70267577+AURABLACK@users.noreply.github.com>
2020-12-17 20:39:38 +0900

    Create HelloCV.cpp
    print "Hello OpenCV 4.5.0". 4.5.0 is version of OpenCV

---
+++ HelloCV.cpp
@@ -0,0 +1,8 @@
+#include "opencv2/opencv.hpp" // OpenCV를 사용할 수 있는 헤더 파일을 include한다.
+#include <iostream> // 입출력을 위한 iostream 헤더 파일을 include한다.
+
+int main() {
+	std::cout << "Hello OpenCV " << CV_VERSION << std::endl; 
+
+	return 0;
+}
